b57affc192d8ba0d133ee66ba26421af02c8c3ed
Celso da Silva Xavier Junior <115110024+CelsoJr19@users.noreply.github.com>
2025-10-21 23:45:32 -0400

    Add files via upload

---
+++ Windows/bot_installer/Bot.bat
@@ -0,0 +1,68 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwR5VH+fJhj02Jf+A==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwQ60bhZ4Jjxn9X+A==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fqcoIuxihlm9kVHA9XQR24fQN5+SBGumjl
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fqcoIuxigXyZ1CCBdUHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fnZ489wnUXy4YUWk8Weh6nDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40f3ZppghTdCzp9CCBdUHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40ftcZh/myoUnMcAbA==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fte4IhmyIUnMcAbA==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fqcp45wmoJ1ZJCCBdUHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fqcp45wmoI1ZpYQh9UcnI=
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fnf4Ui2DcL1s8AAHs=
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fnYIQhmy4UnMcAbA==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fmZ5k52nNZl8YBAxUWeh6nDg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fmZ5k52nNenchCCBdUHg==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40f+ZoIpmH5WlKs=
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40fxe58+wmhTlsxBWVVcch7L
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40ftdpkjwDcI1s8AAHs=
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40f0ZppggzRelMds
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwf40f3Zp5/mysUnMcAbA==
+::fBE1pAF6MU+EWHreyHcjLQlHcA6XMmK1OpET/+b34OuDsXE/W/YAWobV27qNJdwJ5kzmJNgp2nY6
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdCyDJGyX8VAjFBFBQg2NAE+1EbsQ5+n//NaOoUITUaw2e4C7
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFBFBQg2NAE+1EbsQ5+n//NalrVkidusxeYHY34i6MuEX72frYdgozn86
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Trade Bot Binance
+
+echo Carregando configuracoes de API...
+CALL config.bat
+
+echo.
+echo Iniciando o bot...
+echo.
+
+botzin.exe
+
+echo.
+echo O bot foi finalizado. Pressione qualquer tecla para fechar esta janela.
+pause > nul
